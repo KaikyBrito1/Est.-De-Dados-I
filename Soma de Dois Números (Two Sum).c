4a615e18d51275c6471e693fd1978c91006f7b5d
KaikyBrito1 <okaiiky2019@gmail.com>
2026-07-30 22:49:33 -0300

    Refactor Two Sum program for correct input handling
    Fixed array declaration and input handling for target.

--- Soma de Dois Números (Two Sum).c
+++ Soma de Dois Números (Two Sum).c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 
 int main(){
-	int nums[], tamanho;
+	int tamanho;
 	long target;
 	
-	scanf("%d ", &tamanho);
-	scanf("%d", &target);
+	scanf("%d", &tamanho);
+	scanf("%ld", &target);
 	
-	for(int i = 0;i <= tamanho;i++){
-		scanf("%d", nums[i]);
+	int nums[tamanho];
+	
+	for(int i = 0;i < tamanho;i++){
+		scanf("%d", &nums[i]);
 	}
 	for(int i = 0;i < tamanho;i++){
 		for(int j = i+1; j < tamanho;j++){
 			if(nums[i] + nums[j] == target){
 				printf("%d %d", i , j);
-				break;
 			}
 		}
+		break;
 	}
+	
+	
 	return 0;
 }
